e90199dbd955e6565285cfa7f37164d911bb1e28
Dan Waxman <dan.waxman1@gmail.com>
2015-10-25 22:00:12 -0400

    Rewrote and renamed a lot of file, see description
    - Added time argument.
    - Updated usage to accompany time argument.
    - Restyled for global variables. 
    - Added new retrieval system for seconds in time period (now held in a dictionary for easy retrieval of different time modes).
    - Changed the name of the output file to include hours and minutes so files downloaded in hour mode aren't overwritten.

--- download-file-periodically.py
+++ download-file-periodically.py
@@ -9,35 +9,40 @@ import os
 
 ''' 
 I store these as global variables so they can be accessed by every class and there's 
-not chance of using the wrong one
+not chance of using the wrong one.
+These are initialized to None so I can make sure they're specified by the user.
 '''
-file_path = None
-url = None
+FILE_PATH = None
+URL = None
+# Hold time mode -- 'w' is week, 'd' is day, and 'h' is hour.
+# Defaults to week.
+TIME_MODE = 'w'
+# Dictionary that holds seconds during each mode so they can easily be accessed by TIME_DURING[TIME_MODE]
+TIME_DURING = {
+    'w': 604800,
+    'd': 86400,
+    'h': 3600}
 # Made for readability
-seconds_in_week = 604800
-# Made for readability
-usage = '-h/--help -- prints out usage\n-u/--url -- defines url to retrieve file from\n-f/--file -- defines output file path'
+USAGE = '-h/--help -- prints out usage\n-u/--url -- defines url to retrieve file from\n-f/--file -- defines output file path\n-t/--time -- specifies the amount of time between downloads. h is hour, d is day, w is week. Defaults to week.'
 
 
 class GetFiles:
   def __init__(self):
     # declare the global variables to class instance variables for easier access
-    global file_path
-    global url
-    self.file_path = file_path
-    self.url = url
+    self.file_path = FILE_PATH
+    self.url = URL
   def get_file(self):
     '''
     Downloads the file.
     Param free since the script is using global variables.
-    Files are named by the convention master-%weekday-%month-%year.
+    Files are named by the convention master-%weekday-%month-%year-%hour-%minute.
     Next run time is calculated as well and stored in next_run.txt.
     '''
-    self.file_name = time.strftime('master-%a-%b-%d-%Y', time.localtime())
+    self.file_name = time.strftime('master-%a-%b-%d-%Y-%H-%M', time.localtime())
     print('Downloading...')
     urlretrieve(self.url, self.file_path + '/' + self.file_name)
     print('Downloaded.')
-    self.next_run = int(time.time()) + seconds_in_week
+    self.next_run = int(time.time()) + TIME_DURING[TIME_MODE]
     with open('next_run.txt', 'w') as f:
       f.write(str(self.next_run))
     print('Next run on ' + str(self.next_run))
@@ -68,39 +73,48 @@ class MainClass:
     Checks time. If check_time() returns True, get the file.
     Then sleeps for a week + 1 second to avoid possible rounding errors.
     '''
-    global usage
-    global url
-    global file_path
+    global USAGE
+    global URL
+    global FILE_PATH
+    global TIME_MODE
     try:
-      opts, args = getopt.getopt(argv, "u:f:h", ["url=", "file=", "help"])
+      opts, args = getopt.getopt(argv, "u:f:t:h", ["url=", "file=", "time=", "help"])
     except getopt.GetoptError: # Exception for invalid args
-      print(usage)
+      print(USAGE)
       sys.exit(2)
     for opt, arg in opts:
       if opt in ('-h', '--help'):
-        print(usage)
+        print(USAGE)
         sys.exit(2)
       elif opt in ('-u', '--url'):
-        url = arg
-        if not validators.url(url):
+        URL = arg
+        if not validators.url(URL):
           print('Sorry, invalid url!')
           sys.exit(2)
-        print('url set to ' + url)
+        print('url set to ' + URL)
       elif opt in ('-f', '--file'):
-        file_path = arg
-        if not os.path.exists(file_path):
-          os.makedirs(file_path)
-        print('file path set to ' + file_path)
+        FILE_PATH = arg
+        if not os.path.exists(FILE_PATH):
+          os.makedirs(FILE_PATH)
+        print('file path set to ' + FILE_PATH)
+      elif opt in ('-t', '--time'):
+        TIME_MODE = arg
+        if arg not in ('w', 'd', 'h'):
+          print('ERROR: Invalid time argument. Type -h for proper usage.')
+          sys.exit(2)
+        print('Time mode is set to ' + TIME_MODE)
 
     self.get_files = GetFiles()
     self.time_check = TimeCheck()
 
-    if file_path is not None and url is not None:
+    # Global values are all initiated to none. If they're not initiated, they're not specified.
+    if FILE_PATH is not None and URL is not None:
       while True:
         if(self.time_check.check_time() == True):
           self.get_files.get_file()
         print('Sleeping...')
-        time.sleep(seconds_in_week+1) # This is just to avoid weird rounding errors
+
+        time.sleep(TIME_DURING[TIME_MODE] + 1) # This is just to avoid weird rounding errors
     else:
       print('Sorry, file path and url need to be defined. For usage, use the argument -h')
       
